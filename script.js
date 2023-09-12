46c3ae3c87b0b66141003a860f8ff87d5b465377
Andrew Nelson <nelsand19@gmail.com>
2023-09-11 19:15:33 -0600

    start work on the function that adds the number to the display when buttons are clicked

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let content = ""
+
+
 function add (a, b) {
     return a + b
 }
@@ -13,3 +16,25 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b 
 }
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => button.addEventListener("click", (e) => addDigit(e)))
+
+function addDigit (e) {
+    // Setting up clicked button and display
+    const currentButton = e.target.classList.value;
+    const getDisplay = document.querySelector("#display");
+    const content = document.createElement('div')
+    getDisplay.appendChild(content)
+    content.classList.add('content');
+    content.textContent = currentButton;
+
+    // gitDisplay.removeChild
+    
+    // Setting text of display
+    console.log(currentButton);
+    console.log(getDisplay)    
+    
+    // doing math
+    }
