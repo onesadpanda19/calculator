00bbb8413f0e2030c0ba4ad403b79aaae0a6d8fe
Andrew Nelson <nelsand19@gmail.com>
2023-09-22 18:58:00 -0600

    create clear button, start work on equals button

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ const operator = (num1, op, num2) => {
     else if (op === "/") {divide (num1, num2)}
 }
 
+const clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", (e) => clear(e)) 
 
+function clear(e) {
+    values = [];
+    let display = document.querySelector("#display")
+    display.innerHTML = ''
+}
 
 function add (a, b) {
     return a + b
@@ -35,12 +42,27 @@ buttons.forEach((button) => button.addEventListener("click", (e) => addDigit(e))
 function addDigit (e) {
     // Setting up clicked button and display
     const currentButton = e.target.classList.value;
+    if (currentButton === "clear") {
+        return
+    }
     const getDisplay = document.querySelector("#display");
     const content = document.createElement('div')
     getDisplay.appendChild(content)
     content.classList.add('content');
     content.textContent = currentButton;
+    
+    
+
+    // Array of the button values and operator
     values.push(currentButton)
+
+    // 1 - Split array into 3 arrays (one for firstNumber, one for operator, one for secondNumber)
+    // 2 - Join the arrs into strings 
+    // 3 - pass the variables into the operator() function
+    // 4 - display results
+    // 5 - push result back into a new empty value arr for the first num of next operation
+
+
     // gitDisplay.removeChild
 
     // Setting text of display
