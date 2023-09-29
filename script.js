106ddbed551f001b02ab68374a58ebb88cf0fb29
Andrew Nelson <nelsand19@gmail.com>
2023-09-28 18:48:37 -0600

    fix clear and equal buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let values = []
 let math = []
-let solution = []
+let solution = 0
 let firstNum = ""
 let ops = ["+", "-", "/", "*"]
 let secondNum = ""
@@ -23,10 +23,23 @@ const operator = (num1, op, num2) => {
     else if (op === "/") { 
         solution = divide (num1, num2)
     }
+    const getSolution = document.querySelector("#solution");
+    const content = document.createElement('div')
+    getSolution.appendChild(content)
+    content.classList.add('solution')
+    content.textContent = solution;
+
+
     console.log (num1)
     console.log (num2)
     console.log(solution)
     return solution
+
+    // op = op
+    // firstNum = 123
+    // secondNum = 12312
+    // solution = 123124
+    // values = [1,2,3,4,4,1,2]
 }
 
 const clearButton = document.querySelector(".clear")
@@ -34,8 +47,22 @@ clearButton.addEventListener("click", (e) => clear(e))
 
 function clear(e) {
     values = [];
-    let display = document.querySelector("#display")
-    display.innerHTML = ''
+    let display = document.querySelector("#display") // Parent of content
+    let clearSolution = document.querySelector("#solution"); // Parent of solution
+    let contentList = document.querySelectorAll('.content') // List of content
+    let solutionList = document.querySelectorAll(".solution") // List of solutions
+    solutionList.forEach((solution) => {
+        clearSolution.removeChild(solution)
+    })
+     // loop over list of content
+    contentList.forEach((content) => {
+        display.removeChild(content) // remove each content from parent
+    })
+    secondNum = ''
+    firstNum = ''
+    solution = ''
+    op = ''
+    
 }
 
 const equalButton = document.querySelector(".equals")
@@ -49,20 +76,23 @@ function equals(e) {
     }
     else { 
         if (op !== "") {
-        secondNum = secondNum.concat (value) 
-    }
+            secondNum = secondNum.concat (value) 
+        }
         else {
-        firstNum = firstNum.concat (value)
-           }  
+            firstNum = firstNum.concat (value)
+        }  
     }
-
-   
     }) 
 console.log(firstNum)
 console.log(op)
 console.log(secondNum)
     // After loop
 operator(firstNum, op, secondNum)
+
+values = [];
+secondNum = ''
+firstNum = `${solution}`
+ // Cleanup
 }
 
 function add (a, b) {
